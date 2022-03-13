c2eaf7762878c2aec09153ed0e4d7d5703816185
Marton Horvath <17813215+horvathmarton@users.noreply.github.com>
2022-03-13 20:35:01 +0100

    fix: align not found view to center

--- src/app/core/views/not-found/not-found.view.ts
+++ src/app/core/views/not-found/not-found.view.ts
@@ -2,5 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './not-found.view.html',
+  styleUrls: ['./not-found.view.scss'],
 })
 export class NotFoundViewComponent {}
