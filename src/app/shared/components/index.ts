7f3a83975813013e8f5badbc78fd0d3cb09da1cf
Marton Horvath <17813215+horvathmarton@users.noreply.github.com>
2022-04-18 20:21:38 +0200

    refactor: modify portfolio layout

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -2,4 +2,5 @@ export * from './bar-chart/bar-chart.component';
 export * from './content-header/content-header.component';
 export * from './error-message/error-message.component';
 export * from './pie-chart/pie-chart.component';
+export * from './toggleable/toggleable.component';
 export * from './treemap-chart/treemap-chart.component';
