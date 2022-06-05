625ccefbf2fbdbe570a1deb948797aee573f9160
Marton Horvath <17813215+horvathmarton@users.noreply.github.com>
2022-06-05 14:18:52 +0200

    feat: add extra KPIs to the dashboard

--- src/app/dashboard/interfaces/portfolio-indicators.interface.ts
+++ src/app/dashboard/interfaces/portfolio-indicators.interface.ts
@@ -2,4 +2,6 @@ export interface PortfolioIndicators {
   total_aum: number;
   annualized_roic: number;
   annual_dividend_income: number;
+  roic_since_inception: number;
+  total_floating_pnl: number;
 }
