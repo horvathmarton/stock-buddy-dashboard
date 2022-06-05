ddc194d19ed629c83b8328adba68959c7f43786b
Marton Horvath <17813215+horvathmarton@users.noreply.github.com>
2022-06-05 17:30:50 +0200

    build: add Sentry to the project

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import * as Sentry from '@sentry/angular';
+import { BrowserTracing } from '@sentry/tracing';
 import { authPersistStorage } from './app/auth/state';
 import { CoreModule } from './app/core/core.module';
 import { corePersistStorage } from './app/core/state';
 import { environment } from './environments/environment';
+
+Sentry.init({
+  dsn: 'https://47d589340bba44fc98c97f9531cff5e8@o1120245.ingest.sentry.io/6471446',
+  integrations: [
+    new BrowserTracing({
+      tracingOrigins: ['http://localhost:4200', 'https://stock-buddy.com'],
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
+  ],
+  tracesSampleRate: 0.0,
+});
 
 if (environment.production) {
   enableProdMode();
